804c630de948afce229344b825adf7fa1f42a5c0
UsaNB1111 <143878773+UsaNB1111@users.noreply.github.com>
2024-07-10 23:56:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与电脑认知',
   HEO_HERO_TITLE_3: 'MSGZS.CLOUDNRS.ORG',
   HEO_HERO_TITLE_4: '',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: 'https://www.msgzs.cloudns.org/',
+  HEO_HERO_TITLE_LINK: '一起探索！',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
