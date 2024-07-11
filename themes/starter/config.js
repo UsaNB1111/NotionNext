de27c56821c4b886fa50d06e609f2c4c046676ae
UsaNB1111 <143878773+UsaNB1111@users.noreply.github.com>
2024-07-11 23:54:33 +0800

    Create config.js

---
+++ themes/starter/config.js
@@ -0,0 +1,36 @@
+/**
+ * 另一个落地页主题
+ */
+const CONFIG = {
+  // 默认只展示Logo文字，如果设置了logo图片，会在文字左侧显示图标
+  STARTER_LOGO: '', // 普通logo图片 示例：/images/starter/logo/logo.svg
+  STARTER_LOGO_WHITE: '', // 透明底浅色logo 示例： /images/starter/logo/logo-white.svg
+
+  // MENU ， 菜单部分不在此处配置，请在Notion数据库中添加MENU
+
+  // 英雄区块导航
+  STARTER_HERO_ENABLE: true, // 开启英雄区
+  STARTER_HERO_TITLE_1: '此主题已废弃！！！', // 英雄区文字
+  STARTER_HERO_TITLE_2: '此主题已废弃！！！', // 英雄区文字
+  // 英雄区两个按钮，如果TEXT留空则隐藏按钮
+  STARTER_HERO_BUTTON_1_TEXT: '回到首页！', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL:
+    'https://www.msgzs.cloudns.org/', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '回到首页！', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: 'https://www.msgzs.cloudns.org/', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
+
+  STARTER_HERO_PREVIEW_IMAGE: '/public/web.ico', // 产品预览图 ，默认读取public目录下图片
+  
+  // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
+ 
+  // 404页面的提示语
+  STARTER_404_TITLE: '此主题已废弃！！！',
+  STARTER_404_TEXT: '抱歉！您要查找的页面不存在，并此主题已废弃！！！',
+  STARTER_404_BACK: '回到主页',
+
+  STARTER_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
+  STARTER_POST_REDIRECT_URL: 'https://www.msgzs.cloudns.org', // 重定向域名
+  STARTER_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_STARTER_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
+}
+export default CONFIG
