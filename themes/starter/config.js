78728d0ffe5024d0b1ebe1388c3559c19c744e2c
UsaNB1111 <143878773+UsaNB1111@users.noreply.github.com>
2024-07-12 00:01:47 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,14 +10,14 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '此主题已废弃！！！', // 英雄区文字
-  STARTER_HERO_TITLE_2: '此主题已废弃！！！', // 英雄区文字
+  STARTER_HERO_TITLE_1: '欢迎来到我的网站！', // 英雄区文字
+  STARTER_HERO_TITLE_2: '此主题如想探索更多，请点击"继续探索"', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '回到首页！', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_TEXT: '继续探索', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
     'https://www.msgzs.cloudns.org/', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: '回到首页！', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_URL: 'https://www.msgzs.cloudns.org/', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: 'B站主页', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: 'https://space.bilibili.com/168058198', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
 
   STARTER_HERO_PREVIEW_IMAGE: '/public/web.ico', // 产品预览图 ，默认读取public目录下图片
@@ -25,8 +25,8 @@ const CONFIG = {
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
  
   // 404页面的提示语
-  STARTER_404_TITLE: '此主题已废弃！！！',
-  STARTER_404_TEXT: '抱歉！您要查找的页面不存在，并此主题已废弃！！！',
+  STARTER_404_TITLE: 'Oh,No!您想找的东西不存在！',
+  STARTER_404_TEXT: '抱歉！您要查找的页面不存在',
   STARTER_404_BACK: '回到主页',
 
   STARTER_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
